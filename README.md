a4b69b695f93fa948cbb412287146da44f319347
njzwj <njzwj2007@126.com>
2017-03-31 14:06:06 +0800

    using linearSVC with ovr

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 机器学习入门101 对MNIST进行分类
 
+# 学习目标
+
++ 熟悉工作环境，并掌握这种处理数据的方法，提高使用工具的效率。
++ 简单了解MNIST的分类工作，并尝试多种参数比较训练结果的区别。
+
 环境：Windows 10 64bit, anaconda
 数据下载地址[http://yann.lecun.com/exdb/mnist/](http://yann.lecun.com/exdb/mnist/)
 
@@ -23,4 +28,9 @@ nSV = 940, nBSV = 851
 Total nSV = 6873
 done.
 accuracy:  97.99
+```
+
+**2017-03-31 02** 用LinearSVM重新训练了一下。原来sklearn的SVC没有one-vs-the-rest功能，每次训练直接训练45个模型(n-1)*n/2。LinearSVC有ovr，不过最终结果似乎比SVC差一些。这次将LinearSVC的训练结果直接保存到`./dump/default.m`内了，可以使用`joblib`直接载入使用。
+```
+accuracy: 91.16
 ```
--- mnist.py
+++ mnist.py
@@ -1,13 +1,18 @@
 # coding=utf-8
 import struct
+import random
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from PIL import Image
+from sklearn.externals import joblib
 # ==================
 # data preprocessing
 # images in MNIST are 28 * 28 pixels
 # ==================
 
+labels = {}
 # normalize image
 def convert_img(image):
   image = np.array(image)
@@ -48,33 +53,51 @@ def read_label(filename):
   data_set = np.zeros((items, 1))
   for i in range(items):
     item = int(struct.unpack_from('>B', buf, index)[0])
-    if (item != 1 and item != 2):
-      item = 0
     index += struct.calcsize('>B')
     data_set[i, :] = item
   return data_set
 
 # create model
-def create_svm(dataMat, dataLabel, decision='ovr'):
-    clf = SVC(decision_function_shape=decision, verbose = True)
-    clf.fit(dataMat, dataLabel)
-    return clf
+def create_svm(dataMat, dataLabel):
+  clf = LinearSVC(multi_class='ovr', verbose = True)
+  clf.fit(dataMat, dataLabel)
+  return clf
 
 # ====
 # main
 # ====
 def main():
+  # load and train
   train_data = read_image('./data/train-images.idx3-ubyte')
   train_label = read_label('./data/train-labels.idx1-ubyte')
-  
+  print('='*20)
   print('training...')
   svm = create_svm(train_data, train_label.ravel())
   print('done.')
+  joblib.dump(svm, './dump/default.m')
 
-  test_data = read_image('./data/t10k-images.idx3-ubyte')
-  test_label = read_label('./data/t10k-labels.idx1-ubyte')
-
+  # load model(if exists)
+  # svm = joblib.load('./dump/default.m')
+  # test_data = read_image('./data/t10k-images.idx3-ubyte')
+  # test_label = read_label('./data/t10k-labels.idx1-ubyte')
+  
   acc = round(svm.score(test_data, test_label.ravel())*100, 2)
   print('accuracy: ', acc)
+
+  # random test and show
+  for i in range(100):
+    idx = random.random() * 10000
+    img = Image.new('1', (28, 28))
+    for y in range(28):
+      for x in range(28):
+        img.putpixel((x, y), int(test_data[idx][y*28+x]))
+    print('='*20)
+    predict = svm.predict(test_data[idx])
+    print(predict)
+    print(' ')
+    plt.imshow(img, cmap='gray')
+    plt.show()
+
+  
 
 main()
